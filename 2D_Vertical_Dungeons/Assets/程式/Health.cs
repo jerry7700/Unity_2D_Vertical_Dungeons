7c56d331e20a1ca9de4aca661fdbe87d3533003e
jerry7700 <75553251+jerry7700@users.noreply.github.com>
2021-03-08 01:12:29 +0800

    走路研究中

---
+++ 2D_Vertical_Dungeons/Assets/程式/Health.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    #region 欄位
+    [Header("圖騰"),Range(0 , 10)]
+    public int health;
+    [Header("圖騰數量"), Range(1, 10)]
+    public int numOfHearts;
+
+    public Image[] hearts;
+    [Header("滿的圖騰")]
+    public Sprite fullHeart;
+    [Header("空的圖騰")]
+    public Sprite emptyHeart;
+    #endregion
+
+    #region 方法
+    private void Update()
+    {
+        if(health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        //打for快速按tab兩次 會出現for (int i = 0; i < length; i++)
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+
+            if(i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+    #endregion
+}
---
+++ 2D_Vertical_Dungeons/Assets/程式/Player.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 欄位
+    [Header("移動速度")]
+    public float Speed;
+    [Header("")]
+    private Vector2 movement;
+
+    private Animator anim;
+    private Rigidbody2D rb;
+    [Header("水平")]
+    private float h;
+    [Header("垂直")]
+    private float v;
+
+    #endregion
+
+    #region 方法
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    #region 移動
+    private void GetHorizontal()
+    {
+        //輸入取得軸向("水平")
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+    }
+    private void Move()
+    {
+        //剛體.加速度 = 二維(水平 * 速度, 原本加速度.y)
+        rb.velocity = new Vector2(h * Speed, v * Speed);
+
+        //如果玩家按下D就執行
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //transform指的此腳本同一層的 transform元件
+            // Rotation 角度在程式是 localEulerAngless
+            transform.localEulerAngles = Vector3.zero;
+        }
+        //如果玩家按下A就執行
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+        }
+        //如果玩家按下w就執行
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.localEulerAngles = new Vector3(0, -90, 0);
+        }
+        //如果玩家按下s就執行
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.localEulerAngles = new Vector3(0, 90, 0);
+        }
+
+        anim.SetBool("跑步開關", h != 0 || v != 0);
+    }
+    #endregion
+    #endregion
+}
